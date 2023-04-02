108fb9d0b70e0415d147140656128b71240bb33d
waquino <welaquino202122@gmail.com>
2023-04-02 17:42:47 -0300

    feat: added project eletronicStore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,22 +18,22 @@ export default function Headc() {
             >
                <HStack p={'50'} fontSize={{lg: '27px', md: '27px', sm: '27px', base: '20px'}} fontFamily={'montserrat'} justifyContent={'space-around'} flexWrap={'wrap'} spacing={'10'}>
                     <Box>
-                    <Link as={NextLink} href='#projects' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'gray' }} borderRadius={'10px'}>
+                    <Link as={NextLink} href='#projects' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'white',color: 'black'}} borderRadius={'10px'} p={2}>
                        Projetos
                     </Link>
                     </Box>
                     <Box>
-                    <Link as={NextLink} href='#Habilits' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'gray' }} borderRadius={'10px'}>
+                    <Link as={NextLink} href='#Habilits' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'white',color: 'black' }} borderRadius={'10px'} p={2}>
                         Habilidades
                     </Link>
                     </Box>
                     <Box>
-                    <Link as={NextLink} href='#about' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'gray' }} borderRadius={'10px'}>
+                    <Link as={NextLink} href='#about' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'white' ,color: 'black'}} borderRadius={'10px'} p={2}>
                         Sobre
                     </Link>
                     </Box>
                     <Box>
-                    <Link as={NextLink} href='#contact' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'gray' }} borderRadius={'10px'}>
+                    <Link as={NextLink} href='#contact' style={{ textDecoration: 'inherit' }} cursor={'pointer'} _hover={{ bg: 'white' ,color: 'black'}} borderRadius={'10px'} p={2}>
                         Contato
                     </Link>
                     </Box>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,8 +29,22 @@ export default function projects() {
                         </Link>
                         <Text fontSize={{ lg: '35px', md: '35px', sm: '35px', base: '25px' }}>Movie Api</Text>
                     </Box>
-
                     <Box data-aos="flip-left"
+                     w={{lg: '400px',md: '300px', sm: '300px', base: '300px'}} mt={12} h={{md: '250px', lg: '280px', sm: '250px', base: '250px'}} bg={'gray.300'} cursor={'pointer'}
+                        _hover={{
+                            transform: 'scale(1.1)',
+                            transitionDuration: '0.8s',
+                        }}
+                        borderRadius={'20px'}
+                        boxShadow={'dark-lg'}
+                    >
+                        <Link as={NextLink} href='https://eletronic-store-4kor2c3kx-wellingtonaquino22.vercel.app/' target={'_blank'}>
+                            <Image src="eletronic_store.png" h={'full'} borderRadius={'20px'} />
+                        </Link>
+                        <Text fontSize={{ lg: '35px', md: '35px', sm: '35px', base: '25px' }}>Eletronic Store</Text>
+                    </Box>
+
+                    {/* <Box data-aos="flip-left"
                     pt={'110px'}
                      w={{md: '300px', lg: '400px', sm: '300px', base: '300px'}} mt={16} h={{md: '250px', lg: '280px', sm: '250px', base: '250px'}} bg={'gray.200'} cursor={'pointer'} color={'gray.200'} _hover={{
                         transform: 'scale(1.1)',
@@ -38,10 +52,11 @@ export default function projects() {
                     }} borderRadius={'20px'} boxShadow={'dark-lg'}
 
                     >
-                        <Text color={'gray.400'} float='initial' >
-                            EM BREVE 
-                        </Text>
-                    </Box>
+                        <Link as={NextLink} href='https://eletronic-store-4kor2c3kx-wellingtonaquino22.vercel.app/' target={'_blank'}>
+                            <Image src="./eletronic_store.png" h={'full'} borderRadius={'20px'} />
+                        </Link>
+                        <Text fontSize={{ lg: '35px', md: '35px', sm: '35px', base: '25px' }}>Eletronic Store</Text>
+                    </Box> */}
 
                     <Box data-aos="flip-left"
                     pt={'110px'}
